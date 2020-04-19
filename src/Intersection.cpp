6c85d8f5185a1d5911d2aa6bc52111843e6a47d8
dangkhoipham <phamdangkhoi0810@gmail.com>
2020-04-19 10:33:22 +0200

    Feat: integrate traffic light to intersection

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -88,9 +88,11 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
     
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
-
-    //_trafficLight.waitForGreen();
+    
     lck.unlock();
+    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::red){
+        _trafficLight.waitForGreen();
+    }
 }
 
 void Intersection::vehicleHasLeft(std::shared_ptr<Vehicle> vehicle)
@@ -142,12 +144,12 @@ void Intersection::processVehicleQueue()
 bool Intersection::trafficLightIsGreen()
 {
    // please include this part once you have solved the final project tasks
-   /*
+   
    if (_trafficLight.getCurrentPhase() == TrafficLightPhase::green)
        return true;
    else
        return false;
-   */
+   
 
-  return true; // makes traffic light permanently green
+  //return true; // makes traffic light permanently green
 } 
